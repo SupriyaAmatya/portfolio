8e7627d2478c11dbaa69b23b45cdeed95cc09cbd
supriya <linkin.suppu@gmail.com>
2020-06-19 14:07:04 +0545

    Updates

---
+++ precache-manifest.d9598c222f8382d575bda2f9c7048859.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f1d53309f35d753a3b464f3c0b2084",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0df0df9ca899fd9f389b",
+    "url": "/portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "21f7a41d0abd832dd739",
+    "url": "/portfolio/static/css/main.0004dd22.chunk.css"
+  },
+  {
+    "revision": "0df0df9ca899fd9f389b",
+    "url": "/portfolio/static/js/2.3ce81af0.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/portfolio/static/js/2.3ce81af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f7a41d0abd832dd739",
+    "url": "/portfolio/static/js/main.96c920cc.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "2133538917735c55964ebd21289b7de3",
+    "url": "/portfolio/static/media/girl.21335389.png"
+  },
+  {
+    "revision": "5386ebcf76b69a55f6cb771c2cd308aa",
+    "url": "/portfolio/static/media/hand-wave-icon.5386ebcf.png"
+  },
+  {
+    "revision": "504d9ecff1f58bde2dcb173f3cdcf760",
+    "url": "/portfolio/static/media/imagestego.504d9ecf.png"
+  },
+  {
+    "revision": "7a3c0753b08d6f41d61f46406c6ff97a",
+    "url": "/portfolio/static/media/java-icon.7a3c0753.png"
+  },
+  {
+    "revision": "1118d2ba1757bcb975a701b90c94544e",
+    "url": "/portfolio/static/media/leapfrog.1118d2ba.png"
+  },
+  {
+    "revision": "3beac6b1e1e9455acf0e15fa110cc9b0",
+    "url": "/portfolio/static/media/logo.3beac6b1.png"
+  },
+  {
+    "revision": "ce7841f87ee844ec13ef6d11caef5f4e",
+    "url": "/portfolio/static/media/nezuko.ce7841f8.png"
+  },
+  {
+    "revision": "84015bdce79a7d230cf96c371f42afb9",
+    "url": "/portfolio/static/media/oreo.84015bdc.png"
+  },
+  {
+    "revision": "3424b4c0f59301b406aa8688cc5cda80",
+    "url": "/portfolio/static/media/profile.3424b4c0.jpg"
+  },
+  {
+    "revision": "fbf20f513d3c0c4e533d25058157b2d0",
+    "url": "/portfolio/static/media/react-icon.fbf20f51.png"
+  },
+  {
+    "revision": "bb17891573dd652828b0086984cb7eaf",
+    "url": "/portfolio/static/media/slay-queen.bb178915.png"
+  },
+  {
+    "revision": "ee67ffc908e27d263fc77c7a6b0f6c42",
+    "url": "/portfolio/static/media/todo-icon.ee67ffc9.png"
+  },
+  {
+    "revision": "5c00f5ed2f90aa912169a7ac84ff45ba",
+    "url": "/portfolio/static/media/todo.5c00f5ed.png"
+  },
+  {
+    "revision": "5283a0911e540b5a4fdafae484c4a0fa",
+    "url": "/portfolio/static/media/yts.5283a091.png"
+  }
+]);
